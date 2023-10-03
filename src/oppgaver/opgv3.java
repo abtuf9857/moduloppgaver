ae617ff84c3086b9907142c7dc187e327ee0d285
abtuf9857 <143514051+abtuf9857@users.noreply.github.com>
2023-10-03 10:59:25 +0200

    Opv.3

---
+++ src/oppgaver/opgv3.java
@@ -0,0 +1,31 @@
+package oppgaver;
+
+class Person {
+public String navn;
+public String adresse;
+public String telefonnr;
+public int fødtselsår;
+
+public int alder () {
+    return 2023 - fødtselsår;
+ }
+
+ public void skrivUt(int alder){
+    String ut= navn+" med adressen "+adresse+ " med telefonnr "+telefonnr+
+            " er "+alder+" år.";
+     System.out.print(ut);
+ }
+}
+
+public class opgv3 {
+    public static void main(String []args){
+     Person enPerson= new Person();
+     enPerson.navn= " Deebow Wagwan";
+     enPerson.adresse= " Stovner Senter 1";
+     enPerson.telefonnr=" 45767656";
+     enPerson.fødtselsår= 2004;
+     int alder= enPerson.alder();
+     enPerson.skrivUt(alder);
+
+    }
+}
