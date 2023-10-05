e3240a5cbf15a1566764b1dbf80edbf492a81856
abtuf9857 <143514051+abtuf9857@users.noreply.github.com>
2023-10-05 11:49:04 +0200

    Opv.3

--- src/oblig1/oppgave2.java
+++ src/oblig1/oppgave2.java
@@ -1,45 +1,100 @@
 package oblig1;
 
-class vare {
-   private String navn;
+import static javax.swing.JOptionPane.showInputDialog;
+
+class Vare {
+    private String navn;
     private int antall;
     private double pris;
-    public vare(String navn,int antall, double pris){
-        this.navn=navn;
-        this.antall=antall;
-        this.pris=pris;
+
+    //Konstruktør med alle attributter
+    public Vare(String navn, int antall, double pris) {
+        this.navn = navn;
+        this.antall = antall;
+        this.pris = pris;
     }
 
-    public String getNavn(){
+//Get set metoder
+    public String getNavn() {
         return this.navn;
     }
 
-    public void setNavn(String newNavn){
+    public void setNavn(String newNavn) {
         this.navn = newNavn;
     }
-    public int getAntall(){
+
+
+    public int getAntall() {
         return this.antall;
     }
 
-    public void setAntallAntall(int newAntall){
-        this.antall = newAntall;
 
+    public void setAntallAntall(int newAntall) {
+        if (newAntall > 0) {
+            this.antall = newAntall;
+        } else {
+            this.pris = 0;
         }
-    public double getPris(double pris){
-        return this.pris;
     }
 
-    public void setPris(double newPris){
-        this.pris = newPris;
+
+    public double getPris() {
+        return pris;
     }
-          public double totalpris(){
-          return this.pris*this.antall;
-          }
-    public static void main (String []args) {
+
+    public void setPris(double newPris) {
+        if (newPris > 0) {
+            this.pris = newPris;
+        } else {
+            this.pris = 0;
+        }
+
 
     }
+
+    public double totalpris() {
+        return getPris() * getAntall();
+    }
+
+
 }
 
+class main {
+    //navn,antall og pris for vare 1
+    public static void main(String[] args) {
+        String vare1Navn = showInputDialog("Navn til vare er AbdiH");
+        String innAntall1 = showInputDialog("Antall til vare er ");
+        String innPris1 = showInputDialog("Prisen til vare er ");
 
+    //navn,antall og pris for vare 2
+        String vare2Navn = showInputDialog("Navn til andre vare er AbdiS");
+        String antall2 = showInputDialog("Antall til andre er ");
+        String pris2 = showInputDialog("Pris til andre vare er ");
 
+        //To vare objekter for de innleste verdiene
+        //antall1vare,pris1vare,antall2vare og pris2vare er objekter for de innleste verdiene.
+        int antall1vare = 0;
+        double pris1vare = 0;
 
+        int antall2vare=0;
+        double pris2vare=0;
+
+        //Try and catch for å konvertere antall og pris for vare 1 og 2
+        try {
+            antall1vare = Integer.parseInt(innAntall1);
+            pris1vare = Double.parseDouble(innPris1);
+
+            antall2vare=Integer.parseInt(antall2);
+            pris2vare=Double.parseDouble(pris2);
+        } catch (Exception e) {
+            System.out.println("Du skrev inn feil antall eller pris");
+        }
+
+        Vare vare1 = new Vare(vare1Navn, antall1vare, pris1vare);
+        Vare vare2 = new Vare(vare2Navn, antall2vare, pris2vare);
+        System.out.println(vare1.totalpris()+vare2.totalpris());
+        System.out.println("Totalprisen ble:");
+
+    }
+
+}
