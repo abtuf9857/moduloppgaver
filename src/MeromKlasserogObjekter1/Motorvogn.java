ffcbb18664ab406440c2a54dbfe57b494ec1ac58
Tufa <abtuf9857@oslomet.no>
2023-11-16 18:41:41 +0100

    motorvogn

--- src/MeromKlasserogObjekter1/Motorvogn.java
+++ src/MeromKlasserogObjekter1/Motorvogn.java
@@ -1,5 +1,7 @@
 package MeromKlasserogObjekter1;
 
+import java.util.Arrays;
+
 class Dato {
     private int dag, måned, år;
 
@@ -20,21 +22,39 @@ class Dato {
     }
 
     public String toString() {
-    String ut=månedsnavn(måned);// lag denne
+    String ut=dag+"."+" "+månedsnavn(måned)+" "+år;// lag denne
         return ut;
         }
     }
 
 
-class Bil{
+class Bil {
     // opprett følgende attributter
     // typebetegnelse f.eks Audi A6
     // førstegangsregistrert av type Dato
     // kjennetegn dvs. skiltnummer
+    public String typebetegnelse;
+    public Dato førstegangsregistrert;
+    public String kjennetegn;
+
 
     // lag også konstruktør for alle attributtene
+    public Bil(String typebetegnelse, Dato førstegangsregistrert, String kjennetegn){
+        this.typebetegnelse = typebetegnelse;
+        this.førstegangsregistrert = førstegangsregistrert;
+        this.kjennetegn = kjennetegn;
+    }
+
 
     // lag så en toString metode for å vise alle attributtene
+
+public String toString(){
+        String ut="Informasjon om bilen min: \n";
+        ut+="Modellen er : "+this.typebetegnelse +"\n";
+        ut+="Den ble førstegangsregistrert : "+this.førstegangsregistrert +"\n";
+        ut+="Skiltnummeret er : "+this.kjennetegn +"\n";
+        return ut;
+}
 
 }
 
@@ -42,10 +62,22 @@ public class Motorvogn {
 
     public static void main(String[] args) {
         // opprett et array av Biler
-
         // Sett inn noen biler i arrayet
 
+        Bil [] biler = new Bil[4];
+        Bil bil1= new Bil("Audi A6",new Dato(16,11,2020),"TU12");
+        biler[0] = bil1;
+        Bil bil2= new Bil("Merces C63",new Dato(15,11,2021),"TU11");
+        biler[1]=bil2;
+        Bil bil3= new Bil("BMW 5 SERIE",new Dato(17,11,2022),"TU10");
+        biler[2]=bil3;
+        biler[3]=new Bil("Toyota Prius",new Dato(18,11,2023),"Tu9");
+
         // List motorvognregisteret (arrayet) ved hjelp av en løkke
+        for(int i=0;i< biler.length;i++){
+            System.out.println(biler[i]);
+        }
+
 
     }
 }
